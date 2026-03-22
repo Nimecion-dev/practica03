570304902ad793b253b3755a3e5a82310ea54d53
Nimecion-dev <jagudelomancera@gmail.com>
2026-03-22 13:27:31 -0600

    suma

--- suma_y_producto.c
+++ suma_y_producto.c
@@ -1,25 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int pedirEntero(char numero[]){
-	int entero;
-	printf("Inserte el entero %s: ", numero);
-	scanf("%d",&entero);
-	return entero;
-} 
 
-void calcular(){
+int main(int argc, char *argv[])
+{
 	int enteroUno, enteroDos, suma, producto;
-	enteroUno=pedirEntero("uno");
-	enteroDos=pedirEntero("dos");
+	printf("Inserte el entero uno: ");
+	scanf("%d",&enteroUno);
+	printf("Inserte el entero dos: ");
+	scanf("%d",&enteroDos);
 	suma = enteroUno+enteroDos;
 	producto = enteroUno*enteroDos;
 	printf("la suma de los enteros es: %d\n", suma);
 	printf("el producto de los enteros es: %d\n", producto);
 	printf("Este es el fin del programa");
-}
-int main(int argc, char *argv[])
-{
-	calcular();
 	return 0;
 }
