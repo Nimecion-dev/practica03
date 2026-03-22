aa94fbea66622e2d5a7507272fc5a14adda25ea5
Nimecion-dev <jagudelomancera@gmail.com>
2026-03-22 13:41:39 -0600

    valorMonetarioFinal.exe

--- valorMonetario.c
+++ valorMonetario.c
@@ -1,24 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int pedirValor(char mensaje[]){
-	int valor;
-	printf("ingrese la cantidad de %s: ", mensaje);
-	scanf("%d", &valor);
-	return valor;
-}
-
-void darValor(){
-	int cuartos, decimos, quintos, valorMonetario;
-	cuartos = pedirValor("cuartos");
-	decimos = pedirValor("decimos");
-	quintos = pedirValor("quintos");
-	valorMonetario = (25 * cuartos) + (10 * decimos) + (5 * quintos);
-	printf("\nLas monedas valen %d centavos\n", valorMonetario);
-}
-
 int main(int argc, char *argv[])
 {
-	darValor();
+	int cuartos, decimos, quintos, valorMonetario;
+	printf("ingrese la cantidad de cuartos: ");
+	scanf("%d",&cuartos);
+	printf("ingrese la cantidad de cuartos: ");
+	scanf("%d",&decimos);
+	printf("ingrese la cantidad de cuartos: ");
+	scanf("%d",&quintos);
+	valorMonetario = (25 * cuartos) + (10 * decimos) + (5 * quintos);
+	printf("\nLas monedas valen %d centavos\n", valorMonetario);
 	return 0;
 }
